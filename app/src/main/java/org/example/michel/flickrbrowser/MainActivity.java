6cef99a528e85c1fd53cd234ea806998e8de32a9
michel <tensoja@gmail.com>
2015-04-09 20:53:43 -0300

    Get raw data from url

--- app/src/main/java/org/example/michel/flickrbrowser/MainActivity.java
+++ app/src/main/java/org/example/michel/flickrbrowser/MainActivity.java
@@ -1,15 +1,13 @@
 package org.example.michel.flickrbrowser;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,6 +21,9 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        GetRawData theRawData = new GetRawData("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,lollipop&format=json&nojsoncallback=1");
+        
     }
 
 
