11d701e5ab5b9a39295f7f4206cc057923ef27ee
michel <tensoja@gmail.com>
2015-04-13 19:20:11 -0300

    Add parse Json

--- app/src/main/java/org/example/michel/flickrbrowser/GetFlickrJsonData.java
+++ app/src/main/java/org/example/michel/flickrbrowser/GetFlickrJsonData.java
@@ -1,8 +1,13 @@
 package org.example.michel.flickrbrowser;
 
 import android.net.Uri;
-import android.nfc.Tag;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,11 +20,14 @@ public class GetFlickrJsonData extends GetRawData {
     private Uri mDestinationUri;
 
     public GetFlickrJsonData(String searchCriteria, boolean matchAll) {
+
         super(null);
-        createAndUpdateUri(String searchCriteria, boolean matchAll);
+        createAndUpdateUri(searchCriteria, matchAll);
+        mPhotos = new ArrayList<Photo>();
     }
 
     public boolean createAndUpdateUri(String searchCriteria, boolean matchAll) {
+
         final String FLICKR_API_BASE_URL = "https://api.flickr.com/services/feeds/photos_public.gne";
         final String TAGS_PARAM = "tags";
         final String TAGMODE_PARAM = "tagmode";
@@ -35,9 +43,67 @@ public class GetFlickrJsonData extends GetRawData {
         return  mDestinationUri != null;
     }
 
+    public void processResult() {
+
+        if(getmDownloadStatus() != DownloadStatus.OK) {
+            Log.e(LOG_TAG, "Error downloading raw file");
+            return;
+        }
+
+        final String FLICKR_ITEMS = "items";
+        final String FLICKR_TITLE = "title";
+        final String FLICKR_MEDIA = "media";
+        final String FLICKR_PHOTO_URL = "m";
+        final String FLICKR_AUTHOR = "author";
+        final String FLICKR_AUTHOR_ID = "author_id";
+        final String FLICKR_LINK = "link";
+        final String FLICKR_TAGS = "tags";
+
+        try {
+
+            JSONObject jsonData = new JSONObject(getmData());
+            JSONArray itemArray = jsonData.getJSONArray(FLICKR_ITEMS);
+            for(int i=0; i<itemArray.length(); i++) {
+                JSONObject jsonPhoto = itemArray.getJSONObject(i);
+                String title = jsonPhoto.getString(FLICKR_TITLE);
+                String author = jsonPhoto.getString(FLICKR_AUTHOR);
+                String authorId = jsonPhoto.getString(FLICKR_AUTHOR_ID);
+                String link = jsonPhoto.getString(FLICKR_LINK);
+                String tags = jsonPhoto.getString(FLICKR_TAGS);
+
+                JSONObject jsonMedia = jsonPhoto.getJSONObject(FLICKR_MEDIA);
+                String photoUrl = jsonPhoto.getString(FLICKR_PHOTO_URL);
+
+                Photo photoObject = new Photo(title, author, authorId, link, tags, photoUrl);
+
+                this.mPhotos.add(photoObject);
+            }
+
+
+
+        } catch (JSONException jsone) {
+            jsone.printStackTrace();
+            Log.e(LOG_TAG, "Error process Json data");
+        }
+
+
+    }
+
     public class DownloadJsonData extends DownloadRawData {
 
-        
+        protected void onPostExecute(String webData) {
+
+            super.onPostExecute(webData);
+            processResult();
+        }
+
+        protected String doInBackGround(String... params) {
+
+            return super.doInBackground(params);
+
+        }
+
+
     }
 
 }
