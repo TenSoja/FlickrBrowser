1bf248aa6f281ed6dc33e24f216b1563598744f7
michel <tensoja@gmail.com>
2015-04-20 20:48:36 -0300

    Change for extend Base Activity and active the home button

--- app/src/main/java/org/example/michel/flickrbrowser/SearchActivity.java
+++ app/src/main/java/org/example/michel/flickrbrowser/SearchActivity.java
@@ -1,17 +1,17 @@
 package org.example.michel.flickrbrowser;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class SearchActivity extends ActionBarActivity {
+public class SearchActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_search);
+        activateToolbarWithHomeEnabled();
     }
 
 
