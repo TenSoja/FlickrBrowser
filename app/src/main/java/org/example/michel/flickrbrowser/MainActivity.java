bf9cadf5286d3c3dcdd7ebae4f09ba57f0cb677c
michel <tensoja@gmail.com>
2015-04-21 11:42:54 -0300

    Add a onResume method for search keywords

--- app/src/main/java/org/example/michel/flickrbrowser/MainActivity.java
+++ app/src/main/java/org/example/michel/flickrbrowser/MainActivity.java
@@ -1,7 +1,9 @@
 package org.example.michel.flickrbrowser;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
@@ -62,7 +64,25 @@ public class MainActivity extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
-   public class ProcessPhotos extends GetFlickrJsonData {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(flickrRecyblerViewAdapter != null) {
+            String query = getSavedPreferenceData(FLICKR_QUERY);
+            if(query.length() > 0) {
+                ProcessPhotos processPhotos = new ProcessPhotos(query, true);
+                processPhotos.execute();
+            }
+        }
+    }
+
+    private String getSavedPreferenceData(String key) {
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        return sharedPref.getString(key,"");
+    }
+
+    public class ProcessPhotos extends GetFlickrJsonData {
 
        public ProcessPhotos(String searchCriteria, boolean matchAll) {
            super(searchCriteria, matchAll);
