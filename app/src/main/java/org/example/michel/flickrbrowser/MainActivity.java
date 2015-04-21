d20732eb22602a7c5a1f990d1b8fec718eb05307
michel <tensoja@gmail.com>
2015-04-21 11:25:59 -0300

    Add a check for button search

--- app/src/main/java/org/example/michel/flickrbrowser/MainActivity.java
+++ app/src/main/java/org/example/michel/flickrbrowser/MainActivity.java
@@ -1,5 +1,6 @@
 package org.example.michel.flickrbrowser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -50,6 +51,11 @@ public class MainActivity extends BaseActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+        if(id == R.id.menu_search) {
+            Intent intent = new Intent(this, SearchActivity.class);
+            startActivity(intent);
             return true;
         }
 
